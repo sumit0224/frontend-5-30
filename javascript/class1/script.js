9f18e2fd377d57b5a9e714cfc399804ae347d5ac
sumit0224 <sumitgautam969@gmail.com>
2026-02-28 16:07:12 +0530

    javascript starting

---
+++ javascript/class1/script.js
@@ -0,0 +1,77 @@
+
+// variable 
+
+//  var    , let.    , const
+
+// var    redeclear , reassign 
+// let    not redeclear , reassign 
+// const.  not redeclear , not reassign 
+
+
+
+//  data type 
+// primitive data type
+// number , string, boolean , undefine, null, symbol
+
+
+// let a = 10
+// let b = "hello"
+// let c = "hello12"
+// let d = true
+// let e ;
+// let f = null
+// let g = Symbol(10);
+// let h = Symbol(10);
+
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+// console.log(typeof(d))
+// console.log(typeof(e))
+// console.log(typeof(f))
+// console.log(typeof(g))
+// console.log(g==h)
+
+
+// non primitive type
+
+// Array , object, function 
+
+// let arr = [20,15,18,16, 17, 17];
+// console.log(arr[3])
+// let obj = {
+//     name: "sumit"
+// }
+// console.log(obj.name)
+
+
+// oprators
+
+// let a = 20  // assignment op
+
+
+
+
+// + add, - subtract, * multipy, / divide, % modulus reminder,  ** power 
+
+// let a =  10 
+// let b = 23;
+// // console.log(a + b) // 30
+// // console.log(a - b ) // -10
+// // console.log(a*b) // 200
+// // console.log(typeof(a / b)) // 0.5
+// // console.log(a%b) // 10 
+// console.log(a**b) 
+
+// console.log(b%a) // 2
+
+
+// let a = 10;
+// let b = 20;
+
+// // a += 20  // a = a + 20
+//  // a = 10 / 2
+
+// a *= 3
+
+
