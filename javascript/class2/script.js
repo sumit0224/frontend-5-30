18b4670aa05e3b7e4e26bbd1869f643d397c3caf
sumit0224 <sumitgautam969@gmail.com>
2026-03-02 16:06:56 +0530

    condition statements

---
+++ javascript/class2/script.js
@@ -0,0 +1,141 @@
+// logical oprateros 
+// || or  
+//  && and
+//  ! not 
+// ?? nullish
+
+// ==  camparision
+// ===
+
+// let a = "12";
+// let b = 12;
+// console.log(a === b)
+
+// let a = false
+// let b = false
+// console.log(a || b);
+
+// let name = "sumit";
+// let age = 23;
+
+// truthly and falsey value;
+// false , 0 , "", null, undefine, NaN
+
+// console.log( a && b )
+// ! true = // false
+
+
+// console.log(5 > 3 && 10 > 20 && 10 ); // false
+// console.log(5 > 3 || 10 > 20);// true
+// console.log(0 && "Hello"); // 0
+// console.log(1 && "Hello"); // 1
+// console.log("" || "JavaScript"); // javascript
+// console.log(null && undefined); // undefined
+// console.log("JS" && 0 && 100);
+
+// // or || 
+// true || true // true
+// true || false // true
+// false || true // true
+// false || false // false
+
+// true && true // true 1
+// true && false // false
+// false && false // false 1
+// false && true // false 
+
+
+// condition statements
+//  if , if else ,  if else if  else,  switch
+
+
+// let a = 10
+// let b = 15
+
+// if(a > b){
+//     console.log("a is greater then b")
+// }else{
+//     console.log("a is not greater then b ")
+// }
+
+// let num = 11
+// if(num % 2 === 0){
+//     console.log("even")
+// }else{
+//     console.log("odd")
+// }
+
+
+// let age = 17;
+
+// if(age>= 18){
+//     console.log("eligible")
+// }else{
+//     console.log("not eligible")
+// }
+
+
+// let a = 10 ;
+// let b = 20 ;
+
+// if(a> b){
+//     console.log(a)
+// }else{
+//     console.log(b)
+// }
+
+// let num = 11
+// if(num % 5 === 0){
+//     console.log("divisible")
+// }else{
+//     console.log("not divisible")
+// }
+
+// let marks = 87;
+
+// if(marks>90 && marks <=100){
+//     console.log("Grade A")
+// }else if(marks > 80 && marks<= 100){
+//     console.log("Grade B")
+// }else if(marks> 60 && marks<= 100){
+//     console.log("Grade C")
+// }else{
+//     console.log("fail")
+// }
+
+// let a = 10;
+// let b = 25;
+// let c = 5;
+
+// false && true
+// if(a > b && a > c){
+//     console.log(` a = ${a} is greater then b and c`)
+// }else if(b > c){
+//     console.log(`b = ${b} is greater then a and c `)
+// }else{
+//     console.log(`c = ${c} is greater then a and b`)
+// }
+
+// let num1 = 10;
+// let num2 = 5;
+// let operator = "/";
+
+// if(operator === "+"){
+//     console.log(num1 + num2)
+// }else if(operator === "-"){
+//     console.log(num1 - num2)
+// }else if(operator === "*"){
+//     console.log(num1 * num2)
+// }else if(operator === "/"){
+//     console.log(num1 / num2)
+// }
+
+
+// let username = prompt("enter your username : ");
+// let password = prompt("enter your password: ")
+
+// if(username ==="admin" && password ==="1234"){
+//     alert("login succes")
+// }else{
+//     alert("invaild")
+// }
