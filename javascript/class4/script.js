6af69203bc39369d82e262152fa4f7f596fb06a0
sumit0224 <sumitgautam969@gmail.com>
2026-03-10 15:57:06 +0530

    function

---
+++ javascript/class4/script.js
@@ -0,0 +1,120 @@
+// Print numbers from 1 to 10 using a while loop.
+
+// let num = 1;
+
+// while(num<=10){
+//     console.log(num);
+//     num++
+// }
+
+
+// Print numbers from 10 to 1 using a while loop.
+// let num = 10;
+
+// while(num>=1){
+//     console.log(num);
+//     num--
+// }
+
+
+// Print all even numbers from 1 to 20 using a while loop.
+
+// let num = 1;
+// while(num<=20){
+//     if(num%2===0){
+//         console.log(num)
+//     }
+//     num++
+// }
+
+
+// Print all odd numbers from 1 to 20 using a while loop.
+
+// let num = 1;
+// while (num <= 20) {
+//     if (num % 2 !== 0) {
+//         console.log(num)
+//     }
+//     num++
+// }
+
+// Print the multiplication table of 5 using a while loop.
+// let num = 1;
+// while(num<=10){
+//     let value = 5
+//     console.log(`${value} x ${num} = ${num*value}`)
+//     num++
+// }
+
+// Find the sum of numbers from 1 to 100 using a while loop.
+// let sum = 0
+// let num = 1
+// while(num<=100){
+//     sum=sum+num;
+//     num++
+// }
+// console.log(sum)
+
+//  function
+
+// function sayHello(){
+
+//     console.log("hello")
+// }
+// sayHello()
+
+// function sum(xyz, abc, g){
+//     console.log(a)
+// }
+
+// let name = "faraz";
+
+// // arguments
+// sum("faraz", 2, 4)
+
+
+// function allNumberSum(num) {
+//     let sum = 0;
+//     for (let i = 1; i <= num; i++) {
+//         sum = sum + i
+//     }
+//     console.log(sum)
+// }
+
+// allNumberSum(100)
+// allNumberSum(80)
+
+// function isEven(num){
+
+//     if(num%2===0){
+//         console.log("true")
+//     }else{
+//         console.log("false")
+//     }
+
+// }
+// isEven(78)
+// isEven(7)
+
+// void function
+
+// scopes 
+// three types of scopes 
+// 1 . globle scope 
+// 2 functinal scope
+// 3 block scope 
+// let c = 5;
+
+// function b (){
+//     let a = 10 
+//     console.log(c)
+// }
+// let a = 9
+
+// b()
+// console.log(a)
+
+
+// function sum (a, b){
+//     return 
+// }[a, b]
