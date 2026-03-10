0fa77e64db02cf2753d89f6809afb18401445b28
sumit0224 <sumitgautam969@gmail.com>
2026-03-10 14:39:13 +0530

    condition

---
+++ javascript/class3/script.js
@@ -0,0 +1,59 @@
+// nested if else condition 
+// let num = 20;
+
+// if (num > 0) {
+//     console.log("test condition num is greater then 0")
+//     if (num % 2 === 0) {
+//         console.log("jo number even h vo print hoga")
+//         console.log("even")
+//     }else{
+//         console.log("jo number odd h tab print hoga")
+//         console.log("odd")
+//     }
+// }else if(num<0){
+//     console.log("jab number 0 se chota hoga means nagitve namber h ")
+//     console.log("nagitive number")
+// }else{
+//     console.log("na 0 se chota h na zero se bada h to zero hi hoga")
+//     console.log("zero")
+// }
+
+//  loops 
+// let NUM = 2
+// for (let i = 10; i >= 0; i--) {
+// console.log(`${NUM} X ${i} = ${NUM*i}`)
+// }
+
+// let sum = 0
+// for(let i = 1; i<= 100; i++){
+
+//     console.log(`${sum} + ${i}`)
+//     sum = sum + i
+//     console.log(`updated sum value ${sum} `)
+// }
+// console.log(sum)
+
+// let factorial = 1
+// let num = 5
+
+// for(let i = 1; i<=num ; i++){
+    
+//     factorial = factorial * i
+//     console.log(`${factorial}`)
+    
+// }
+// clear
+//  while loop
+
+// let password = prompt("enter your password ")
+
+// while (password !== "1234"){
+//     password = prompt("retry ")
+// }
+// console.log("correct password")
+// do while
+
+// do{
+// password = prompt("enter your password")
+// }while(password !=="1234")
+
